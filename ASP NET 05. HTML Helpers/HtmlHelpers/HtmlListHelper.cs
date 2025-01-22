4444b9d9fd8a0a3d9dfa09d37acbe6d76543f7d5
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-01-22 16:34:47 +0400

    HTML Helpers

---
+++ ASP NET 05. HTML Helpers/HtmlHelpers/HtmlListHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text;
+
+namespace ASP_NET_05._HTML_Helpers.HtmlHelpers;
+
+public static class HtmlListHelper
+{
+    public static HtmlString ListFor(
+                            this IHtmlHelper helper,
+                            IEnumerable<object> items,
+                            string listTag = "ul",
+                            string color="black",
+                            string fontSize="16")
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"<{listTag} " +
+            $"style='color: {color};" +
+            $"font-size:{fontSize}px'>");
+        foreach(var item in items)
+        {
+            sb.AppendLine($"<li>{item}</li>");
+        }
+        sb.AppendLine($"</{listTag}>");
+
+        return new HtmlString(sb.ToString());
+    }
+}
---
+++ ASP NET 05. HTML Helpers/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP_NET_05._HTML_Helpers.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}. {Login}";
+    }
+}
